86f2da3a94c0e6b5575b37414690b3888f75c923
Freddy Meijer <meijerfw@gmail.com>
2017-07-01 13:09:01 +0200

    Corrected models

--- db/migrate/20170701110259_create_members.rb
+++ db/migrate/20170701110259_create_members.rb
@@ -1,7 +1,6 @@
 class CreateMembers < ActiveRecord::Migration[5.1]
   def change
     create_table :members do |t|
-      t.string :team
       t.string :name
       t.string :project_link
       t.string :captain
--- db/migrate/20170701110815_create_teams.rb
+++ db/migrate/20170701110815_create_teams.rb
@@ -4,9 +4,11 @@ class CreateTeams < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :description
       t.string :project_link
-      t.string :members
       t.string :logo
-      t.integer :score
+      t.integer :concept_score
+      t.integer :style_score
+      t.integer :git_score
+      t.integer :userfriendly_score
 
       t.timestamps
     end
