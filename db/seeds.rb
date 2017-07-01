420f4229ccef4c8d1fb999b995bf38113e4accd1
Freddy Meijer <meijerfw@gmail.com>
2017-07-01 13:31:35 +0200

    editted model files for relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+team1 = Team.create!({
+  name: "Team Awesome",
+  project_link: "github link",
+  description: "A hackathon site",
+  logo: "",
+  concept_score: 0,
+  style_score: 0,
+  git_score: 0,
+  userfriendly_score: 0,
+  })
+
+team2 = Team.create!({
+  name: "Team Yay",
+  project_link: "github link",
+  description: "A hackathon site",
+  logo: "",
+  concept_score: 0,
+  style_score: 0,
+  git_score: 0,
+  userfriendly_score: 0,
+  })
+
+  member1 = Member.create!({
+    name: "",
+    github "",
+    captain: ""
+    })
